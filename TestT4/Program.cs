20764a2f3f98eb9d6841873519a677fbb4e2225a
fenghao <fenghao@seecool.cc>
2018-03-22 17:57:52 +0800

    init

--- TestT4/Program.cs
+++ TestT4/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,17 @@ namespace TestT4
     {
         static void Main(string[] args)
         {
+            using (MySqlConnection conn = new MySqlConnection("Server=192.168.14.124;Port=3306;Database=test1;Uid=root;Pwd=root;Character Set=utf8;"))
+            {
+                //using (MySqlDataAdapter adapter = new MySqlDataAdapter("select * from yz_ship_info", conn))
+                //{
+                //    DataTable dt = new DataTable();
+                //    adapter.Fill(dt);
+                //    Console.WriteLine(dt.Rows.Count);
+                //}
+                MySqlCommand cmd = new MySqlCommand("", conn);
+                cmd.ExecuteReader();
+            }
         }
     }
 }
