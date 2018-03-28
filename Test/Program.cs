dd4c7760f1c2844d41088b723d234db6ce3f13ca
fenghao <fenghao@seecool.cc>
2018-03-28 18:38:52 +0800

    update wpf

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,6 +10,34 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            //test1();
+            test2();
+        }
+
+        private static void test2()
+        {
+            string str1 = formatPara(new { Id = 1, Name = "test2" });
+            Test1 t1 = new Test1() { Id = 1, Name = "test2" };
+            string str2 = formatPara(t1);
+        }
+        class Test1
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        static string formatPara(object para)
+        {
+            if (para == null)
+                return string.Empty;
+            else
+            {
+                var dic = new System.Web.Routing.RouteValueDictionary(para);
+                return $"?{string.Join("&", dic.Select(p => p.Key + "=" + p.Value))}";
+            }
+        }
+
+        static void test1()
+        {
             string str = Guid.NewGuid().ToString();
             Console.WriteLine(str);
         }
